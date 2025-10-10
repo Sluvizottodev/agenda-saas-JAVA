58555eca46be9237ca1c1cae9749cb90556f383f
stefani.luvizotto <stefani.luvizotto@a4pm.com.br>
2025-10-10 11:42:30 -0300

    feat: remove classes PestradorService e RepostasUtil como obsoletas, mantendo compatibilidade

--- agenda-saas/src/main/java/br/cefet/agenda-saas/utils/RepostasUtil.java
+++ agenda-saas/src/main/java/br/cefet/agenda-saas/utils/RepostasUtil.java
@@ -1,9 +1,5 @@
 package br.cefet.agendaSaas.utils;
 
-/**
- * @deprecated Use {@link RespostasUtil} instead. Classe mantida temporariamente para compatibilidade
- */
-@Deprecated
+/
 public class RepostasUtil {
-	// classe vazia mantida apenas para compatibilidade com versões antigas
 }
